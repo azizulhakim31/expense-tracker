bd2aa2bad1326086036df1c01eacd021b9b64acf
Md. Azizul Hakim <aazizul924@gmail.com>
2026-08-27 23:32:25 +0600

    DOM element selection

---
+++ script.js
@@ -0,0 +1,14 @@
+const transactionForm = document.getElementById("transaction_form");
+const descriptionInput = document.getElementById("description");
+const amountInput = document.getElementById("amount");
+const typeInput = document.getElementById("type");
+const categoryInput = document.getElementById("category");
+const dateInput = document.getElementById("date");
+
+const transactionList = document.getElementById("transaction_list");
+const emptyMessage = document.getElementById("empty_message");
+const searchInput = document.getElementById("search");
+
+const balanceElement = document.getElementById("balance");
+const incomeElement = document.getElementById("income");
+const expenseElement = document.getElementById("expense");
